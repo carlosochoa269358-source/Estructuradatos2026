1cdc88cab749b4cdd6936ecd00293c5d82ca5c85
ANDRES OCHOA HERRERA <carlosochoa269358@correo.itm.edu.co>
2026-02-18 15:44:04 -0500

    complete

---
+++ repos2026/multiplo/Program.cs
@@ -0,0 +1,28 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primer numero: ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo numero: ");
+    if (b % a == 0)
+    {
+        Console.WriteLine($"El numero {a} es multiplo de: {b}");
+    }
+    else
+    {
+        Console.WriteLine($"El numero {a} No es multiplo de: {b}");
+    }
+
+{
+    answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase))) ;
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
+
+Console.WriteLine("Game Over");
+
+
+
