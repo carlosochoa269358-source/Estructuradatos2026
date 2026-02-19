87b87d49a62b03fe4539a9a08924614e1b9a120d
ANDRES OCHOA HERRERA <carlosochoa269358@correo.itm.edu.co>
2026-02-18 23:20:16 -0500

    is leap yer

---
+++ repos2026/bisiest/Program.cs
@@ -0,0 +1,51 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtension.GetInt("Ingrese año: ");
+
+    if (year == currentYear)
+    {
+        message = "es";
+    }
+    else if (year > currentYear)
+    {
+        message = "va a ser";
+    }
+    else
+    {
+        message = "fue";
+    }
+
+    if (year % 4 == 0)
+
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($"El año {year} Si {message} bisiesto");
+            }
+            else
+            {
+                Console.WriteLine($"El año {year} No {message} bisiesto");
+
+            }
+        }
+        else
+        {
+            Console.WriteLine($"El año {year} Si {message} bisiesto");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine($"El año {year} No {message} bisiesto");
+    }
+} while (true);  
+   
