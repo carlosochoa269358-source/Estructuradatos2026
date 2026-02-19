a20e0d7be0d0fa9fa3707af0a6ae5d5395cadc77
ANDRES OCHOA HERRERA <carlosochoa269358@correo.itm.edu.co>
2026-02-19 01:26:40 -0500

    complete

---
+++ repos2026/Salario/Program.cs
@@ -0,0 +1,35 @@
+﻿using Shared;
+using System;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+
+    var name = ConsoleExtension.Getstring("Ingrese nombre........................: ");
+    var workhours = ConsoleExtension.GetFloat("Ingrese numero de horas trabajadas....: ");
+    var hourValue = ConsoleExtension.GetDecimal("Ingrese valor hora....................: ");
+    var salaryMinimum = ConsoleExtension.GetDecimal("Ingrese valor salario mínimo mensual: ");
+
+    var salary = (decimal)workhours * hourValue;
+    if (salary < salaryMinimum)
+    {
+        Console.WriteLine($"Nombre...............................: {name}");
+        Console.WriteLine($"Salario............................: {salaryMinimum:C2}");
+    }
+    else
+    {
+        Console.WriteLine($"Nombre.................................: {name}");
+        Console.WriteLine($"Salario...............................: {salary:C2}");
+    }
+
+     do
+     {
+         answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+     } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over");
+
--- repos2026/Shared/Class1.cs
+++ repos2026/Shared/Class1.cs
@@ -24,4 +24,33 @@ public static class ConsoleExtension
         }
         return null;
     }
+
+    public static string? Getstring(string message)
+    {
+        Console.Write(message);
+        var text = Console.ReadLine();
+        return text;
+    }
+    public static float GetFloat(string message)
+    {
+        Console.Write(message);
+        var numberString = Console.ReadLine();
+        if (float.TryParse(numberString, out float numberFloat))
+        {
+            return numberFloat;
+        }
+
+        return 0;
+    }
+    public static decimal GetDecimal(string message)
+    { 
+        Console.Write(message);
+        var numberString = Console.ReadLine();
+        if (decimal.TryParse(numberString, out decimal numberDecimal))
+        {
+            return numberDecimal;
+        }
+
+        return 0;
+    }
 }
